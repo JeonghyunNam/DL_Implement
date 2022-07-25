d517df2d77a8ee90f98184aae05be48e015f4076
JeonghyunNam <ys4990@kaist.ac.kr>
2022-07-25 21:13:03 +0900

    UNET completed

--- UNET/data.py
+++ UNET/data.py
@@ -13,16 +13,18 @@ class CustomDataset(d.Dataset):
     def __init__(self, path, train = True):
         self.path = path    # Input path until train/val
         self.img_path = path +'/Image/'
-        self.label_path = path + '/Label/'
+        self.label_path = path + '/label/'
+        self.weight_path = path +'/weights/'
         self.train = train
         self.img_list = [self.img_path+k for k in os.listdir(self.img_path)]
         self.label_list = [self.label_path + k for k in os.listdir(self.label_path)]
+        self.weight_list = [self.weight_path + k for k in os.listdir(self.weight_path)]
 
     def __len__(self):
         return len(self.img_list)
     
     
-    def __transform__(self, image, mask):
+    def __transform__(self, image, mask, weight):
         """
             Image Augmentation
             Have to be changed, but works well
@@ -31,11 +33,13 @@ class CustomDataset(d.Dataset):
         if random.random() > 0.5:
             image = F.hflip(image)
             mask = F.hflip(mask)
+            weight = F.hflip(weight)
 
         # Random vertical flipping
         if random.random() > 0.5:
             image = F.vflip(image)
             mask = F.vflip(mask)
+            weight = F.vflip(weight)
         
         # Random rotating (have to be chaged)
         # if random.random() > 0.5:
@@ -46,7 +50,7 @@ class CustomDataset(d.Dataset):
         # Elastic distortion (have to be changed)
         # if random.random() > 0.5:
         #     image.gaussian_distortion(probability=1, grid_width=3, grid_height=3, magnitude=5, corner='bell', method='in')
-        return image, mask
+        return image, mask, weight
 
 
     def __getitem__(self, idx):
@@ -55,15 +59,17 @@ class CustomDataset(d.Dataset):
         """
         img_path = self.img_list[idx]
         label_path = self.label_list[idx]
-        img, label = Image.open(img_path), Image.open(label_path)
+        weight_path = self.weight_list[idx]
+        img, label, weight = Image.open(img_path), Image.open(label_path), Image.open(weight_path)
         if self.train:
-            img, label = self.__transform__(img, label)
+            img, label, weight = self.__transform__(img, label, weight)
         
         # Transform to tensor
         img = F.to_tensor(img)
         label = F.to_tensor(label)
+        weight = F.to_tensor(weight)
         
-        return img, label
+        return img, label, weight
 
 if __name__ == '__main__':
     """
